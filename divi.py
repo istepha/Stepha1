1f5aea939768a61574687abbd9bba6a00d084d1c
istepha <istepha.farfan@gmail.com>
2017-07-13 12:07:15 -0500

    Hola

---
+++ divi.py
@@ -0,0 +1,7 @@
+#Comentario
+print ("programa que suma dos números")
+a= int (input ("Ingresa el numero 1: "))
+b= int (input ("Ingresa el numero 2: "))
+div = a - b
+print ("El resultado es" , div)
+
---
+++ suma.py
@@ -0,0 +1,7 @@
+#Comentario
+print ("programa que suma dos números")
+a= int (input ("Ingresa el sumando 1: "))
+b= int (input ("Ingresa el sumando 2: "))
+suma = a + b
+print ("El resultado es" , suma)
+
